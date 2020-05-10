fc2883d9c2c167b99085d108843de524bc93ea18
shuGeorgio <dev.shudiscrete@gmail.com>
2020-05-10 16:57:11 +0900

    initial commt

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  text = "Default statement";
+
+  constructor() {}
+
+  onChangeText() {
+    if (this.text == "changed") {
+      this.text = "chstatementanged";
+    } else {
+      this.text = "changed";
+    }
+  }
+}
